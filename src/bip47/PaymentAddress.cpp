ba44f8cfba1a6854130ea7bc99cb955168aead26
top1st <riguang102@gmail.com>
2019-10-28 04:50:28 -0700

    LogPrint sG

--- src/bip47/PaymentAddress.cpp
+++ src/bip47/PaymentAddress.cpp
@@ -104,6 +104,7 @@ CPubKey PaymentAddress::getSendECKey(Scalar s)
     LogPrintf("getSendECKey:ecPoint = %s\n", ecPoint.GetHex());
     
     GroupElement sG = get_sG(s);
+    LogPrintf("getSendECKey:sG = %s\n", ecPoint.GetHex());
     GroupElement ecG = ecPoint + sG;
     LogPrintf("getSendECKey:ecG= %s\n", ecG.GetHex());
     LogPrintf("getSendECKey:buffersize required = %d\n", ecG.memoryRequired());
